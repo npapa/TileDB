bdecdbd1a5c40da5fb8f4a8308c16d7ffa0916f8
Nikos Papailiou <npapa@cslab.ntua.gr>
2017-08-02 22:08:18 +0300

    clang

--- scripts/install-travis-deps.sh
+++ scripts/install-travis-deps.sh
@@ -21,6 +21,6 @@ sudo cmake --build . --target install
 
 # Install clang-format
 sudo add-apt-repository 'deb http://apt.llvm.org/trusty/ llvm-toolchain-trusty-5.0 main'
-wget -O - http://apt.llvm.org/llvm-snapshot.gpg.key|sudo apt-key add -
+wget -O - https://apt.llvm.org/llvm-snapshot.gpg.key|sudo apt-key add -
 sudo apt-get update -qq 
 sudo apt-get install -qq -y clang-format-5.0
