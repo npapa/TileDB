b0d548fcaf2ccb67d1bcccaa3ce280d3e06aae71
Nikos Papailiou <npapa@cslab.ntua.gr>
2017-08-04 09:50:12 +0300

    test

--- test/src/unit-libhdfs.cc
+++ test/src/unit-libhdfs.cc
@@ -167,18 +167,18 @@ int get_hosts(const char *fileName) {
 
 TEST_CASE_METHOD(LibHDFSFx, "Test hdfs integration") {
    totalSize=1000000000;
-   check_hdfs_write("/tmp/testFile_rw",totalSize,4096);
-   check_hdfs_read("/tmp/testFile_rw",4096);
+   check_hdfs_write("testFile_rw",totalSize,4096);
+   check_hdfs_read("testFile_rw",4096);
 
-   check_hdfs_write("/tmp/testFile_rw",totalSize,1048576);
-   check_hdfs_read("/tmp/testFile_rw",1048576);
+   check_hdfs_write("testFile_rw",totalSize,1048576);
+   check_hdfs_read("testFile_rw",1048576);
 
-   check_hdfs_write("/tmp/testFile_rw",totalSize,10485760);
-   check_hdfs_read("/tmp/testFile_rw",10485760);
+   check_hdfs_write("testFile_rw",totalSize,10485760);
+   check_hdfs_read("testFile_rw",10485760);
 
-   check_hdfs_write("/tmp/testFile_rw",totalSize,104857600);
-   check_hdfs_read("/tmp/testFile_rw",104857600);
+   check_hdfs_write("testFile_rw",totalSize,104857600);
+   check_hdfs_read("testFile_rw",104857600);
 
-   get_hosts("/tmp/testFile_rw");
+   get_hosts("testFile_rw");
 }
 
