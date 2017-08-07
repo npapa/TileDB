aa5c04f718db5dd78dca3a56b913e76f032563c5
Nikos Papailiou <npapa@cslab.ntua.gr>
2017-08-07 15:53:35 +0300

    test

--- scripts/install-hadoop.sh
+++ scripts/install-hadoop.sh
@@ -13,7 +13,7 @@ function install_java {
 function install_hadoop {
   sudo mkdir -p /usr/local/hadoop/
   cd /usr/local/hadoop
-  sudo curl http://apache.forthnet.gr/hadoop/common/hadoop-2.8.1/hadoop-2.8.1.tar.gz | sudo tar xz 
+  sudo curl http://apache.forthnet.gr/hadoop/common/hadoop-3.0.0-alpha4/hadoop-3.0.0-alpha4.tar.gz | sudo tar xz 
   sudo mv hadoop-2.8.1 home
   sudo chown -R hadoop /usr/local/hadoop
 }
