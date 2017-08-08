2ae33e1a5cb1a170ce1005ccb2b03de77b16de40
Nikos Papailiou <npapa@cslab.ntua.gr>
2017-08-08 09:40:48 +0300

    test

--- scripts/install-hadoop.sh
+++ scripts/install-hadoop.sh
@@ -157,7 +157,6 @@ export HADOOP_HOME=/usr/local/hadoop/home
 echo "HADOOP_HOME: " $HADOOP_HOME
 export HADOOP_LIB="$HADOOP_HOME/lib/native/"
 echo "HADOOP_LIB: " $HADOOP_LIB
-ls $JAVA_HOME/jre/lib/amd64/server/
 export LD_LIBRARY_PATH="$HADOOP_LIB:$JAVA_HOME/lib/amd64/server/"
 echo "LD_LIBRARY_PATH: " $LD_LIBRARY_PATH
 export CLASSPATH=`$HADOOP_HOME/bin/hadoop classpath --glob`
